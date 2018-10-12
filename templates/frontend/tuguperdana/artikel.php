25763424dec7769132bfc9c806439e97721a9413
asepchenko <33862578+asepchenko@users.noreply.github.com>
2018-10-12 09:35:49 +0700

    menambahkan file kategori dan artikel di frontend

---
+++ templates/frontend/tuguperdana/artikel.php
@@ -0,0 +1,283 @@
+<?php get_template('header');?> 
+
+
+  <!-- Start Bottom Header -->
+  <div class="header-bg page-area">
+    <div class="home-overly"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+          <div class="slider-content text-center">
+            <div class="header-bottom">
+              <div class="layer2 wow zoomIn" data-wow-duration="1s" data-wow-delay=".4s">
+                <h1 class="title2">Detail Layanan Kami </h1>
+              </div>
+              <div class="layer3 wow zoomInUp" data-wow-duration="2s" data-wow-delay="1s">
+                <h2 class="title3">(Nama Layanan / Services Yang Dipilih)</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- END Header -->
+
+  <div class="blog-page area-padding">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+          <div class="page-head-blog">
+            <div class="single-blog-page">
+              <!-- search option start -->
+              <form action="#">
+                <div class="search-option">
+                  <input type="text" placeholder="Search...">
+                  <button class="button" type="submit">
+                                            <i class="fa fa-search"></i>
+                                        </button>
+                </div>
+              </form>
+              <!-- search option end -->
+            </div>
+            <div class="single-blog-page">
+              <!-- recent start -->
+              <div class="left-blog">
+                <h4>Layanan Kami Lainnya</h4>
+                <div class="recent-post">
+                  <!-- start single post -->
+                  <div class="recent-single-post">
+                    <div class="post-img">
+                      <a href="#">
+                           <img src="img/blog/1.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="pst-content">
+                      <p><a href="#">Perindustrian & Perdagangan.</a></p>
+                    </div>
+                  </div>
+                  <!-- End single post -->
+                  <!-- start single post -->
+                  <div class="recent-single-post">
+                    <div class="post-img">
+                      <a href="#">
+                           <img src="img/blog/2.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="pst-content">
+                      <p><a href="#"> Telematika.</a></p>
+                    </div>
+                  </div>
+                  <!-- End single post -->
+                  <!-- start single post -->
+                  <div class="recent-single-post">
+                    <div class="post-img">
+                      <a href="#">
+                           <img src="img/blog/3.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="pst-content">
+                      <p><a href="#"> Jasa Survey.</a></p>
+                    </div>
+                  </div>
+                  <!-- End single post -->
+                  <!-- start single post -->
+                  <div class="recent-single-post">
+                    <div class="post-img">
+                      <a href="#">
+                           <img src="img/blog/4.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="pst-content">
+                      <p><a href="#"> Studi Penelitian dan Bantuan Teknis.</a></p>
+                    </div>
+                  </div>
+                  <!-- End single post -->
+                  <!-- start single post -->
+                  <div class="recent-single-post">
+                    <div class="post-img">
+                      <a href="#">
+                           <img src="img/blog/1.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="pst-content">
+                      <p><a href="#">Pendidikan.</a></p>
+                    </div>
+                  </div>
+                  <!-- End single post -->
+                  <!-- start single post -->
+                  <div class="recent-single-post">
+                    <div class="post-img">
+                      <a href="#">
+                           <img src="img/blog/2.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="pst-content">
+                      <p><a href="#"> Transportasi.</a></p>
+                    </div>
+                  </div>
+                  <!-- End single post -->
+                  <!-- start single post -->
+                  <div class="recent-single-post">
+                    <div class="post-img">
+                      <a href="#">
+                           <img src="img/blog/3.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="pst-content">
+                      <p><a href="#"> Pengembangan Pertanian dan Pedesaan.</a></p>
+                    </div>
+                  </div>
+                  <!-- End single post -->
+                  <!-- start single post -->
+                  <div class="recent-single-post">
+                    <div class="post-img">
+                      <a href="#">
+                           <img src="img/blog/4.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="pst-content">
+                      <p><a href="#"> Kepariwisataan.</a></p>
+                    </div>
+                  </div>
+                  <!-- End single post -->
+                  <!-- start single post -->
+                  <div class="recent-single-post">
+                    <div class="post-img">
+                      <a href="#">
+                           <img src="img/blog/1.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="pst-content">
+                      <p><a href="#">Jasa Analisis Engineering.</a></p>
+                    </div>
+                  </div>
+                  <!-- End single post -->
+                  <!-- start single post -->
+                  <div class="recent-single-post">
+                    <div class="post-img">
+                      <a href="#">
+                           <img src="img/blog/2.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="pst-content">
+                      <p><a href="#"> Tata Lingkungan.</a></p>
+                    </div>
+                  </div>
+                  <!-- End single post -->
+                  <!-- start single post -->
+                  <div class="recent-single-post">
+                    <div class="post-img">
+                      <a href="#">
+                           <img src="img/blog/3.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="pst-content">
+                      <p><a href="#"> Bidang Sipil.</a></p>
+                    </div>
+                  </div>
+                  <!-- End single post -->
+                  <!-- start single post -->
+                  <div class="recent-single-post">
+                    <div class="post-img">
+                      <a href="#">
+                           <img src="img/blog/4.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="pst-content">
+                      <p><a href="#"> Jasa Arsitektur.</a></p>
+                    </div>
+                  </div>
+                  <!-- End single post -->
+                </div>
+              </div>
+              <!-- recent end -->
+            </div>
+            <div class="single-blog-page">
+              <div class="left-tags blog-tags">
+                <div class="popular-tag left-side-tags left-blog">
+                  <h4>popular tags</h4>
+                  <ul>
+                    <li>
+                      <a href="#">Portfolio</a>
+                    </li>
+                    <li>
+                      <a href="#">Project</a>
+                    </li>
+                    <li>
+                      <a href="#">Design</a>
+                    </li>
+                    <li>
+                      <a href="#">wordpress</a>
+                    </li>
+                    <li>
+                      <a href="#">Joomla</a>
+                    </li>
+                    <li>
+                      <a href="#">Html</a>
+                    </li>
+                    <li>
+                      <a href="#">Masonry</a>
+                    </li>
+                    <li>
+                      <a href="#">Website</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- End left sidebar -->
+        <!-- Start single blog -->
+        <div class="col-md-8 col-sm-8 col-xs-12">
+          <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+              <!-- single-blog start -->
+              <article class="blog-post-wrapper">
+                <div class="post-thumbnail">
+                  <img src="img/blog/6.jpg" alt="" />
+                </div>
+                <div class="post-information">
+                  <h2>Title Nama Service yang dipilih dari page layanan-kami</h2>
+                  <div class="entry-meta">
+                    <span class="author-meta"><i class="fa fa-user"></i> <a href="#">admin</a></span>
+                    <span><i class="fa fa-clock-o"></i> march 28, 2016</span>
+                    <span class="tag-meta">
+                        <i class="fa fa-folder-o"></i>
+                        <a href="#">painting</a>,
+                        <a href="#">work</a>
+                      </span>
+                    <span>
+                        <i class="fa fa-tags"></i>
+                        <a href="#">tools</a>,
+                        <a href="#"> Humer</a>,
+                        <a href="#">House</a>
+                      </span>
+                    <span><i class="fa fa-comments-o"></i> <a href="#">6 comments</a></span>
+                  </div>
+                  <div class="entry-content">
+                    <p>Aliquam et metus pharetra, bibendum massa nec, fermentum odio. Nunc id leo ultrices, mollis ligula in, finibus tortor. Mauris eu dui ut lectus fermentum eleifend. Pellentesque faucibus sem ante, non malesuada odio varius nec. Suspendisse
+                      potenti. Proin consectetur aliquam odio nec fringilla. Sed interdum at justo in efficitur. Vivamus gravida volutpat sodales. Fusce ornare sit amet ligula condimentum sagittis.</p>
+                    <blockquote>
+                      <p>Quisque semper nunc vitae erat pellentesque, ac placerat arcu consectetur. In venenatis elit ac ultrices convallis. Duis est nisi, tincidunt ac urna sed, cursus blandit lectus. In ullamcorper sit amet ligula ut eleifend. Proin dictum
+                        tempor ligula, ac feugiat metus. Sed finibus tortor eu scelerisque scelerisque.</p>
+                    </blockquote>
+                    <p>Aenean et tempor eros, vitae sollicitudin velit. Etiam varius enim nec quam tempor, sed efficitur ex ultrices. Phasellus pretium est vel dui vestibulum condimentum. Aenean nec suscipit nibh. Phasellus nec lacus id arcu facilisis elementum.
+                      Curabitur lobortis, elit ut elementum congue, erat ex bibendum odio, nec iaculis lacus sem non lorem. Duis suscipit metus ante, sed convallis quam posuere quis. Ut tincidunt eleifend odio, ac fringilla mi vehicula nec. Nunc vitae
+                      lacus eget lectus imperdiet tempus sed in dui. Nam molestie magna at risus consectetur, placerat suscipit justo dignissim. Sed vitae fringilla enim, nec ullamcorper arcu.</p>
+                  </div>
+                </div>
+              </article>
+              <div class="clear"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- End Blog Area -->
+  <div class="clearfix"></div>
+
+
+<?php get_template('footer');?>
---
+++ templates/frontend/tuguperdana/kategori_artikel.php
@@ -0,0 +1,254 @@
+<?php get_template('header');?> 
+
+  <!-- Start Bottom Header -->
+  <div class="header-bg page-area">
+    <div class="home-overly"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+          <div class="slider-content text-center">
+            <div class="header-bottom">
+              <div class="layer2 wow zoomIn" data-wow-duration="1s" data-wow-delay=".4s">
+                <h1 class="title2">Layanan Kami</h1>
+              </div>
+              <div class="layer3 wow zoomInUp" data-wow-duration="2s" data-wow-delay="1s">
+                <h2 class="title3">PT. Tugu Perdana</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- END Header -->
+
+  <!-- Start Service area -->
+  <div id="services" class="services-area area-padding">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+          <div class="section-headline services-head text-center">
+            <h2>Layanan Kami</h2>
+          </div>
+        </div>
+      </div>
+      <div class="row text-center">
+        <div class="services-contents">
+          <!-- Start Left services -->
+          <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class="about-move">
+              <div class="services-details">
+                <div class="single-services">
+                  <a class="services-icon" href="layanan-detail.html">
+                      <i class="fa fa-building"></i>
+                    </a>
+                  <h4>Jasa Arsitektur</h4>
+                  <p>
+                    Penjelasan singkat,, dan icon bisa di klik utk kebutuhan penjelasan detail mengenai layanan terkait.
+                  </p>
+                </div>
+              </div>
+              <!-- end about-details -->
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class="about-move">
+              <div class="services-details">
+                <div class="single-services">
+                  <a class="services-icon" href="layanan-detail.html">
+                      <i class="fa fa-group"></i>
+                    </a>
+                  <h4>Bidang Sipil</h4>
+                  <p>
+                    Penjelasan singkat,, dan icon bisa di klik utk kebutuhan penjelasan detail mengenai layanan terkait.
+                  </p>
+                </div>
+              </div>
+              <!-- end about-details -->
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-4 col-xs-12">
+            <!-- end col-md-4 -->
+            <div class=" about-move">
+              <div class="services-details">
+                <div class="single-services">
+                  <a class="services-icon" href="layanan-detail.html">
+                      <i class="fa fa-object-group"></i>
+                    </a>
+                  <h4>Tata Lingkungan</h4>
+                  <p>
+                    Penjelasan singkat,, dan icon bisa di klik utk kebutuhan penjelasan detail mengenai layanan terkait.
+                  </p>
+                </div>
+              </div>
+              <!-- end about-details -->
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-4 col-xs-12">
+            <!-- end col-md-4 -->
+            <div class=" about-move">
+              <div class="services-details">
+                <div class="single-services">
+                  <a class="services-icon" href="layanan-detail.html">
+                      <i class="fa fa-gears"></i>
+                    </a>
+                  <h4>Jasa Analisis Engineering </h4>
+                  <p>
+                    Penjelasan singkat,, dan icon bisa di klik utk kebutuhan penjelasan detail mengenai layanan terkait.
+                  </p>
+                </div>
+              </div>
+              <!-- end about-details -->
+            </div>
+          </div>
+          <!-- End Left services -->
+          <div class="col-md-4 col-sm-4 col-xs-12">
+            <!-- end col-md-4 -->
+            <div class=" about-move">
+              <div class="services-details">
+                <div class="single-services">
+                  <a class="services-icon" href="layanan-detail.html">
+                      <i class="fa fa-camera-retro"></i>
+                    </a>
+                  <h4>Kepariwisataan </h4>
+                  <p>
+                    Penjelasan singkat,, dan icon bisa di klik utk kebutuhan penjelasan detail mengenai layanan terkait.
+                  </p>
+                </div>
+              </div>
+              <!-- end about-details -->
+            </div>
+          </div>
+          
+          <div class="col-md-4 col-sm-4 col-xs-12">
+            <!-- end col-md-4 -->
+            <div class=" about-move">
+              <div class="services-details">
+                <div class="single-services">
+                  <a class="services-icon" href="layanan-detail.html">
+                      <i class="fa fa-line-chart"></i>
+                    </a>
+                  <h4>Pengembangan Pertanian dan Pedesaan</h4>
+                  <p>
+                    Penjelasan singkat,, dan icon bisa di klik utk kebutuhan penjelasan detail mengenai layanan terkait.
+                  </p>
+                </div>
+              </div>
+              <!-- end about-details -->
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-4 col-xs-12">
+            <!-- end col-md-4 -->
+            <div class=" about-move">
+              <div class="services-details">
+                <div class="single-services">
+                  <a class="services-icon" href="layanan-detail.html">
+                      <i class="fa fa-subway"></i>
+                    </a>
+                  <h4>Transportasi </h4>
+                  <p>
+                    Penjelasan singkat,, dan icon bisa di klik utk kebutuhan penjelasan detail mengenai layanan terkait.
+                  </p>
+                </div>
+              </div>
+              <!-- end about-details -->
+            </div>
+          </div>
+          <!-- End Left services -->
+          <div class="col-md-4 col-sm-4 col-xs-12">
+            <!-- end col-md-4 -->
+            <div class=" about-move">
+              <div class="services-details">
+                <div class="single-services">
+                  <a class="services-icon" href="layanan-detail.html">
+                      <i class="fa fa-graduation-cap"></i>
+                    </a>
+                  <h4>Pendidikan </h4>
+                  <p>
+                    Penjelasan singkat,, dan icon bisa di klik utk kebutuhan penjelasan detail mengenai layanan terkait.
+                  </p>
+                </div>
+              </div>
+              <!-- end about-details -->
+            </div>
+          </div>
+          
+          <div class="col-md-4 col-sm-4 col-xs-12">
+            <!-- end col-md-4 -->
+            <div class=" about-move">
+              <div class="services-details">
+                <div class="single-services">
+                  <a class="services-icon" href="layanan-detail.html">
+                      <i class="fa fa-credit-card"></i>
+                    </a>
+                  <h4>Perindustrian & Perdagangan</h4>
+                  <p>
+                    Penjelasan singkat,, dan icon bisa di klik utk kebutuhan penjelasan detail mengenai layanan terkait.
+                  </p>
+                </div>
+              </div>
+              <!-- end about-details -->
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-4 col-xs-12">
+            <!-- end col-md-4 -->
+            <div class=" about-move">
+              <div class="services-details">
+                <div class="single-services">
+                  <a class="services-icon" href="layanan-detail.html">
+                      <i class="fa fa-desktop"></i>
+                    </a>
+                  <h4>Telematika </h4>
+                  <p>
+                    Penjelasan singkat,, dan icon bisa di klik utk kebutuhan penjelasan detail mengenai layanan terkait.
+                  </p>
+                </div>
+              </div>
+              <!-- end about-details -->
+            </div>
+          </div>
+          <!-- End Left services -->
+          <div class="col-md-4 col-sm-4 col-xs-12">
+            <!-- end col-md-4 -->
+            <div class=" about-move">
+              <div class="services-details">
+                <div class="single-services">
+                  <a class="services-icon" href="layanan-detail.html">
+                      <i class="fa fa-pie-chart"></i>
+                    </a>
+                  <h4>Jasa Survey </h4>
+                  <p>
+                    Penjelasan singkat,, dan icon bisa di klik utk kebutuhan penjelasan detail mengenai layanan terkait.
+                  </p>
+                </div>
+              </div>
+              <!-- end about-details -->
+            </div>
+          </div>
+          
+          <div class="col-md-4 col-sm-4 col-xs-12">
+            <!-- end col-md-4 -->
+            <div class=" about-move">
+              <div class="services-details">
+                <div class="single-services">
+                  <a class="services-icon" href="layanan-detail.html">
+                      <i class="fa fa-file-text"></i>
+                    </a>
+                  <h4>Studi Penelitian dan Bantuan Teknis</h4>
+                  <p>
+                    Penjelasan singkat,, dan icon bisa di klik utk kebutuhan penjelasan detail mengenai layanan terkait.
+                  </p>
+                </div>
+              </div>
+              <!-- end about-details -->
+            </div>
+          </div>
+        </div> <!-- End Left services -->
+      </div>
+    </div>
+  </div>
+  <!-- End Service area -->
+
+  <div class="clearfix"></div>
+
+<?php get_template('footer');?>
